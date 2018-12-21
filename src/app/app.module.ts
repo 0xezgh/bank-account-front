25318d31a2fc5f6ac27b0a3593e062b6ba79944a
ezdin <ezdine.gharbi@gmail.com>
2018-12-21 12:10:49 +0100

    generated page not found module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OperationsComponent } from './operations/operations.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OperationsComponent
+    OperationsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
